0fc06762a347b2b7052aa0a66442dd62ae31b4a0
Achraf SP <achraf.sp@gmail.com>
2018-07-08 13:29:58 +0200

    Add get/todo/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -26,6 +27,25 @@ Todo.find().then((todos)=>{
   res.send({todos});
 }, (e)=>{
  res.status(400).send(e);
+});
+});
+
+// Get // todos/12345
+app.get('/todos/:id', (req, res)=>{
+  //res.send(req.params);
+  var id = req.params.id;
+  // check id is valid
+  if(!ObjectID.isValid(id)){
+    return res.status(400).send();
+  }
+
+Todo.findById(id).then((todo)=>{
+  if(!todo){
+    res.status(400).send();
+  }
+  res.send({todo});
+}).catch((e)=>{
+  res.status(400).send();
 });
 });
 
