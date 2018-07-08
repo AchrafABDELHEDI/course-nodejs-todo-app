3413b3ca31121723fdb6ec61baf7021f6323ebea
Achraf SP <achraf.sp@gmail.com>
2018-07-08 18:12:05 +0200

    Add delete todo

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+var {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result)=>{
+//   console.log(result);
+// });
+
+//Todo.findOneAndRemove()
+//Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('5b422cb23b71123f782a6d9e').then((todo)=>{
+  console.log(todo);
+});
